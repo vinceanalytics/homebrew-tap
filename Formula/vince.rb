7f6adbb01f324c9b053441975c07c9886b5ed650
Geofrey Ernest <geofreyernest@live.com>
2024-03-21 17:02:58 +0300

    Brew formula update for vince version v0.0.67

--- Formula/vince.rb
+++ Formula/vince.rb
@@ -5,21 +5,21 @@
 class Vince < Formula
   desc "API first high performance self hosted and cost effective privacy friendly web analytics  server for organizations of any size"
   homepage "https://vinceanalytics.com"
-  version "0.0.66"
+  version "0.0.67"
   license "Apache 2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/vinceanalytics/vince/releases/download/v0.0.66/vince_darwin_arm64.tar.gz"
-      sha256 "5dd90e1eb4580664ad15af4600386f90123a5482e5c0def25655a4d7e7af63aa"
+      url "https://github.com/vinceanalytics/vince/releases/download/v0.0.67/vince_darwin_arm64.tar.gz"
+      sha256 "cdaf7a7c2f1be3a755702d79cae3a09f04af36a7ab1c59d81f5ba9f6841af813"
 
       def install
         bin.install "vince"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/vinceanalytics/vince/releases/download/v0.0.66/vince_darwin_x86_64.tar.gz"
-      sha256 "5c47060952bb66a5f2e39a9b4a615d4d60e4ed3205fcdb5738d17860ad8d7dce"
+      url "https://github.com/vinceanalytics/vince/releases/download/v0.0.67/vince_darwin_x86_64.tar.gz"
+      sha256 "f3a17c8670a3768f4ea7adb0f8193914a0444d96b67a4fd865eff49f2fe62234"
 
       def install
         bin.install "vince"
@@ -29,16 +29,16 @@ class Vince < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/vinceanalytics/vince/releases/download/v0.0.66/vince_linux_arm64.tar.gz"
-      sha256 "f6411493990a86cf52962974f31d0a883eef048251c6531a87eca34c914fbc63"
+      url "https://github.com/vinceanalytics/vince/releases/download/v0.0.67/vince_linux_arm64.tar.gz"
+      sha256 "0475f872d66acbd9495c63b8f102ef1fef9b9b55df98284827065a5d38698ebc"
 
       def install
         bin.install "vince"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/vinceanalytics/vince/releases/download/v0.0.66/vince_linux_x86_64.tar.gz"
-      sha256 "dddce1aec8d6dcc4db3b99b2025a00cc677d287dd87e469dd4e6d3524f1c9637"
+      url "https://github.com/vinceanalytics/vince/releases/download/v0.0.67/vince_linux_x86_64.tar.gz"
+      sha256 "68f42f654b8042211c895a9e6c52841ed68841f4febcf2afe33bbc9d497d431b"
 
       def install
         bin.install "vince"
